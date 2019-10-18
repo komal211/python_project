5e35ba10412dc0c852591002725c0b81b8fc50e5
komal211 <45573632+komal211@users.noreply.github.com>
2019-10-18 18:52:55 -0400

    Update game_car.py

--- game_car.py
+++ game_car.py
@@ -1,10 +1,19 @@
 command = ""
+started = False
 while True:
     command = input("> ").lower()
     if command.lower() == "start":
-        print("Car  has been started.....")
+        if started:
+            print("Car already started")
+        else:
+            started=True
+            print("Car  has been started.....")
     elif command.lower() == "stop":
-        print("Car stopped.")
+        if not started:
+            print("car is already stopped")
+        else:
+            started = False
+            print("Car stopped.")
     elif command == "help":
         print("""
 start - start car
